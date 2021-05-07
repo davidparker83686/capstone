9e2d167cb4711009b844a87a12a8fdbba4ef368e
Lane <lanericharddavis@gmail.com>
2021-05-07 16:32:48 -0600

    Landing Page router set

---
+++ capstone.client/src/pages/SearchPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="component">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchPage',
+  setup() {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
