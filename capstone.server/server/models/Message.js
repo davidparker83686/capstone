e9471a3610fed48297a676666663677bc3b9e092
davidparker83686 <davidjparker91@gmail.com>
2021-05-07 12:06:31 -0600

    message

---
+++ capstone.server/server/models/Message.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+const Message = new Schema(
+  {
+    from: { type: String, required: true },
+    to: { type: String, required: true },
+    body: { type: String, required: true },
+    description: { type: String, required: true },
+    creatorId: { type: String, ref: 'Account', required: true }
+  },
+  { timestamps: true, toJSON: { virtuals: true } }
+)
+
+Message.virtual('creator', {
+  localField: 'creatorId',
+  ref: 'Account',
+  foreignField: '_id',
+  justOne: true
+})
+
+export default Message
