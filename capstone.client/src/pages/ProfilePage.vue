e73f95246aa14cd8e11ceff579089eab2ef6858c
Scott <76458930+IDMiller2020@users.noreply.github.com>
2021-05-11 10:23:31 -0600

    scott morning progress

--- capstone.client/src/pages/ProfilePage.vue
+++ capstone.client/src/pages/ProfilePage.vue
@@ -19,7 +19,6 @@
             </h5>
             <div>
               <img class="img-fluid rounded mb-2" :src="state.user.picture" alt="profile picture">
-              {{ currentProfile }}
             </div>
             <div class=" d-none d-md-block">
               <div class="card mb-2">
@@ -101,11 +100,9 @@ export default {
       items: computed(() => AppState.items),
       reviews: computed(() => AppState.reviews),
       account: computed(() => AppState.account),
-      user: computed(() => AppState.user),
-      currentProfile: computed(() => AppState.currentProfile)
+      user: computed(() => AppState.user)
     })
     onMounted(async() => {
-      AppState.currentProfile = AppState.account
       try {
         await itemsService.getItemsByUserId(route.params.id)
         // await reviewsService.getReviewsByUserId(route.params.id)
