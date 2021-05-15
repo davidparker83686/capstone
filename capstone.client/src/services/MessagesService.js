3c00971a34b02462a3ae6579b9426f8b1e110b52
davidparker83686 <davidjparker91@gmail.com>
2021-05-14 21:13:27 -0600

    removed logs

--- capstone.client/src/services/MessagesService.js
+++ capstone.client/src/services/MessagesService.js
@@ -1,13 +1,12 @@
 // import { AppState } from '../AppState'
 
 import { AppState } from '../AppState'
-import { logger } from '../utils/Logger'
 import { api } from './AxiosService'
 
 class MessagesService {
   async getAllMessages(id) {
     const res = await api.get(`api/profile/${id}/messages`)
-    logger.log(res.data)
+
     // AppState.messages = res.data.filter(message => (message.creatorId === id && message.to === AppState.account.id) || (message.creatorId === AppState.account.id && message.to === id))
     // AppState.messages = res.data.filter(message => (message.creatorId === id) || (message.creatorId === AppState.account.id))
     AppState.messages = res.data
